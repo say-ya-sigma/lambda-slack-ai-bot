e7e87600be2ba5f1a1c08db3606ff198e4ad3637
sigma <seiya.sugimoto.free@gmail.com>
2024-06-14 03:49:41 +0900

    cut out slack keywords var to another file

---
+++ slack_keyword.py
@@ -0,0 +1,17 @@
+BOT_CURSOR = ":robot_face:"
+KEYWORD_IMAGE = "描いて"
+MSG_PREVIOUS = "以前の会話内容を確認中……" + BOT_CURSOR
+MSG_IMAGE_DESCRIBE = "画像を読み込み中……" + BOT_CURSOR
+MSG_IMAGE_GENERATE = "画像生成の準備中……" + BOT_CURSOR
+MSG_IMAGE_DRAW = "画像を描画中……" + BOT_CURSOR
+MSG_RESPONSE = "応答を待機中……" + BOT_CURSOR
+COMMAND_DESCRIBE = "Describe the image in great detail as if viewing a photo."
+COMMAND_GENERATE = "Convert the above sentence into a command for DALL-E to generate an image within 1000 characters. Just give me a prompt."
+
+CONVERSION_ARRAY = [
+    ["**", "*"],
+    ["#### ", "🔸 "],
+    ["### ", "🔶 "],
+    ["## ", "🟠 "],
+    ["# ", "🟡 "],
+]
